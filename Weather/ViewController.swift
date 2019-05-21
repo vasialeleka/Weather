9d3d0e33c66afd39265f9bf9f902c761e305e76b
Vasia Leleka <vasialeleka@ukr.net>
2019-05-21 09:53:49 -0700

    url, get location

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -9,20 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
- @IBOutlet weak var searchBar: UISearchBar!
+
+    @IBOutlet weak var searchBar: UISearchBar!
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+           searchBar.delegate = self
     }
-
-    extension ViewController: UISearchBarDelegate
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-   
     
 }
+  extension ViewController: UISearchBarDelegate {
 
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        var locationMain : String?
+        print(searchBar.text!)
+        let urlString = "https://api.apixu.com/v1/current.json?key=6d5e4821328f498aade150303192105&q="+searchBar.text!
+        let url = URL(string: urlString)
+        let task = URLSession.shared.dataTask(with: url!){(data, response, error) in
+            do{
+                let json = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as! [String : AnyObject]
+                if let location  = json["location"] {
+                    locationMain = location["name"] as? String
+                }
+            } catch let jsonError{
+                print(jsonError)
+            }
+        }
+            task.resume()
+        
+    }
+
+}
