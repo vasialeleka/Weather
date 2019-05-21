14d39f6a8e571a1ebbb8bb246c6ad19363cf0331
Vasia Leleka <vasialeleka@ukr.net>
2019-05-21 08:43:12 -0700

    add search bar

---
+++ Weather/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Vasia Leleka on 21.05.2019.
+//  Copyright © 2019 Vasia Leleka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+ @IBOutlet weak var searchBar: UISearchBar!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+    extension ViewController: UISearchBarDelegate
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+   
+    
+}
+
